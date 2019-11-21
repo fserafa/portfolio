d461406ade46e95a210fbc6c9024bb421bb84e72
Felipe <felipe.seraf@gmail.com>
2019-11-21 19:34:46 -0300

    Updates

---
+++ precache-manifest.b4c35ac5d1c2bf66427e407daeaaf004.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "242a658ccfec74396c737284b33e801e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e1b24616898bbc30843c",
+    "url": "/portfolio/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "38932d0714b8b8da1657",
+    "url": "/portfolio/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "e1b24616898bbc30843c",
+    "url": "/portfolio/static/js/2.7416e3f9.chunk.js"
+  },
+  {
+    "revision": "38932d0714b8b8da1657",
+    "url": "/portfolio/static/js/main.ff739e8a.chunk.js"
+  },
+  {
+    "revision": "6b26cb3bc1b9147ba086",
+    "url": "/portfolio/static/js/runtime-main.86a05020.js"
+  },
+  {
+    "revision": "75435ed15923602b7d759343ce273ce6",
+    "url": "/portfolio/static/media/headerbg.75435ed1.jpg"
+  }
+]);
